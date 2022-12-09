bbc78879ed5a88895c02ab1f4ef9099e587aa709
ashprice <48685381+ashprice@users.noreply.github.com>
2022-12-09 15:40:29 +0000

    fixing currentSum subtraction

--- askdf.jl
+++ askdf.jl
@@ -1,9 +1,7 @@
 targetPercent = 0.985 * 150000
 item1 = 0.07 * 150000
-
-currentSum = item1
-
-numItems = 1
+global currentSum = item1
+global numItems = 1
 
 while currentSum < targetPercent
     nextItem = item1 / numItems
@@ -13,11 +11,15 @@ while currentSum < targetPercent
     end
 
     global currentSum += nextItem
-    if currentSum == 21000
-        currentSum -= 10500
+    if round(currentSum) == 21000.00
+        global currentSum -= 10500
     end
 
     global numItems += 1
+
+    if numItems == 2
+        println(currentSum)
+    end
 end
 
 println(numItems)
