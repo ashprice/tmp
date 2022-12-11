be5bb6f8ff4d59cc63129488b5d7fd3ced77e763
ashprice <48685381+ashprice@users.noreply.github.com>
2022-12-11 01:09:13 +0000

    unshitted

--- askdf.jl
+++ askdf.jl
@@ -1,26 +1,38 @@
-targetPercent = 0.985 * 150000
-item1 = 0.07 * 150000
-global currentSum = item1
-global numItems = 1
+# more pseudoscience at work
+alpha = 1.13
 
-while currentSum < targetPercent
-    nextItem = item1 / numItems
-    minItem = 1.00
-    if nextItem < minItem
-        nextItem = minItem
+n = 35735475
+
+target_cum_frequency = (0.98 * 35735475) 
+
+item1 = 0.06055 * n
+
+cum_frequency = 0
+num_items = 1
+frequency = (0.06055 * n)
+
+for i in 1:n
+    if num_items == 1
+        global cum_frequency += item1
+    else
+        global frequency = (item1 / (num_items ^ alpha))
+        if frequency <= 1
+            global frequency = 1
+        end
+        if num_words >= 200
+            global frequency = (item1 / (num_items ^ 1))
+        end
+        if num_words >= 2000
+            global frequency = (item1 / (num_items ^ 0.8))
+        end
+        global cum_frequency += frequency
     end
-
-    global currentSum += nextItem
-    if round(currentSum) == 21000.00
-        global currentSum -= 10500
-    end
-
-    global numItems += 1
-
-    if numItems == 2
-        println(currentSum)
+    global num_items += 1
+    if cum_frequency >= target_cum_frequency
+        println("Number of words: ", num_items)
+        println("Frequency: ", cum_frequency)
+        break
     end
 end
 
-println(numItems)
 
